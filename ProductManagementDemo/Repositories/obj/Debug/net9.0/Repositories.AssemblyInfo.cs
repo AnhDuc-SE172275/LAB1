bc7400dd7d5875c35ade506c3dcda5bad53f1620
PRN212-SE1855 <linhcuro123@gmial.com>
2025-06-30 10:13:01 +0700

    LAB1

--- ProductManagementDemo/Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
+++ ProductManagementDemo/Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3ed4212fdce8dd7ea24221dce99da429c9d98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
